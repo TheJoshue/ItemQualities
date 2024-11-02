ddfba271e364b15fcc1ce9cf06ff58a29872ef6f
W1CK3D <w1ck3dworks@gmail.com>
2024-11-02 18:53:14 +0100

    Update QualitiesManager.java

--- src/main/java/com/abraxas/itemqualities/QualitiesManager.java
+++ src/main/java/com/abraxas/itemqualities/QualitiesManager.java
@@ -514,13 +514,14 @@ public class QualitiesManager {
             && quality.itemWhitelistMode
             && !quality.itemList.contains(itemStack.getType()));
 
+        Utils.log("itemQualities.isEmpty() - " + itemQualities.isEmpty());
+        Utils.log("itemQualities - " + itemQualities);
+        Utils.log("return - " + (itemQualities.isEmpty() ? null : itemQualities.get(Utils.getRandom().nextInt(itemQualities.size()))));
+
         for (ItemQuality quality : itemQualities) {
             if (chanceOf(quality.addToItemChance)) return quality;
         }
 
-        Utils.log("itemQualities.isEmpty() - " + itemQualities.isEmpty());
-        Utils.log("itemQualities - " + itemQualities);
-        Utils.log("return - " + (itemQualities.isEmpty() ? null : itemQualities.get(Utils.getRandom().nextInt(itemQualities.size()))));
         return itemQualities.isEmpty() ? null : itemQualities.get(Utils.getRandom().nextInt(itemQualities.size()));
     }
 
